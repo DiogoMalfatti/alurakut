09e958b43f44346484a85423a12952b21aa04b43
Diogo Malfatti <diimalfatti@gmail.com>
2021-07-12 14:44:47 -0300

    feito a estrutura basica da home

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  /* --> RESET CSS ABRE */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  /* RESET CSS FECHA */
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serif;
+    background: #D9E6F6;
+  }
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
 `
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-gap: 10px;
+  padding: 16px;
+  @media (min-width: 860px) {
+    grid-template-columns: 160px 1fr 312px;
+    grid-template-areas: 'profileArea welcomeArea profileRelationsArea';
+  }
+`
+const Box = styled.div`
+  background: #fff;
+  border-radius: 8px;
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <MainGrid>
+      <div style={{ gridArea: 'profileArea' }}>
+        <Box>Imagem</Box>
+      </div>
+      <div style={{ gridArea: 'welcomeArea' }}>
+        <Box>BemVindo</Box>
+      </div>
+      <div style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>Amigos</Box>
+        <Box>Comunidades</Box>
+      </div>
+    </MainGrid>
+  )
 }
