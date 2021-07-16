1b0823d82571edd1fd5347c9c7cfb39a82876ba9
Diogo Malfatti <diimalfatti@gmail.com>
2021-07-16 11:36:10 -0300

    finalizado projeto e criado login

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import {
@@ -50,8 +52,8 @@ function ProfileRelationsBox(props) {
   )
 }
 
-export default function Home() {
-  const githubUser = 'diogomalfatti'
+export default function Home(props) {
+  const githubUser = props.githubUser
   const [comunidades, setComunidades] = React.useState([])
   const pessoasFavoritas = [
     'juunegreiros',
@@ -197,4 +199,35 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN
+
+  const { isAuthenticated } = await fetch(
+    'https://alurakut.vercel.app/api/auth',
+    {
+      headers: {
+        Authorization: token,
+      },
+    },
+  ).then(resposta => resposta.json())
+
+  console.log(isAuthenticated)
+
+  if (!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    }
+  }
+  const { githubUser } = jwt.decode(token)
+  return {
+    props: {
+      githubUser,
+    }, // will be passed to the page component as props
+  }
 }
